2541a33aa9a9305acb1c4457d12608c53b9d655a
Aaron <baronfirestorm@gmail.com>
2024-06-05 00:15:43 +0100

    Finished waypoint system to automatically detect other waypoints.

--- Assets/Scripts/WaypointMap.cs
+++ Assets/Scripts/WaypointMap.cs
@@ -8,11 +8,12 @@ public class WaypointMap : MonoBehaviour
     [SerializeField] List<GameObject> waypoints = new List<GameObject>();
     [SerializeField] float threshold;
     
-    float limitX = 30f;
-    float limitZ = 30f;
-    [SerializeField] LayerMask wallMask;
-    Color color = new Color(255f, 255f, 255f);
-
+    float limitXPos = 30f, limitXNeg = -30f, limitZPos = 30f, limitZNeg = -30f;
+    
+    GameObject xPosWaypoint = null, xNegWaypoint = null, zPosWaypoint = null, zNegWaypoint = null; 
+    [SerializeField] LayerMask wallMask, waypointMask;
+    Color white = new Color(255f, 255f, 255f);
+    Color red = new Color(255f, 0f, 0f);
     // Start is called before the first frame update
     void Start()
     {
@@ -47,62 +48,50 @@ public class WaypointMap : MonoBehaviour
                 if (waypoint == waypointComparison) continue;
 
                 float waypointDistance = (waypoint.transform.position - waypointComparison.transform.position).magnitude;
+                Vector3 waypointDirection = (waypointComparison.transform.position - waypoint.transform.position);
                 if (waypointDistance < threshold)
                 {
                     RaycastHit hit;
-                    if (!Physics.Raycast(waypoint.transform.position, waypointComparison.transform.position, out hit, wallMask))
-                    {
-                        Debug.DrawLine(waypoint.transform.position, waypointComparison.transform.position, color, 1000f);
+                    if (!Physics.Raycast(waypoint.transform.position, waypointDirection, out hit, wallMask))
+                    {   
+                        if (Physics.Raycast(waypoint.transform.position, transform.right, out hit, waypointMask))
+                        {
+                            if (hit.transform.tag == "Waypoint")
+                                xPosWaypoint = hit.transform.gameObject;
+                        }
+                        if (Physics.Raycast(waypoint.transform.position, -transform.right, out hit, waypointMask))
+                        {
+                            if (hit.transform.tag == "Waypoint")
+                                xNegWaypoint = hit.transform.gameObject;
+                        }
+                        if (Physics.Raycast(waypoint.transform.position, transform.forward, out hit, waypointMask))
+                        {
+                            if (hit.transform.tag == "Waypoint")
+                                zPosWaypoint = hit.transform.gameObject;
+                        }
+                        if (Physics.Raycast(waypoint.transform.position, -transform.forward, out hit, waypointMask))
+                        {
+                            if (hit.transform.tag == "Waypoint")
+                                zNegWaypoint = hit.transform.gameObject;
+                        }
                     }
                 }
             }
-            /*
-            // Gets the individual waypoints in the list, casts a box cast on them, gets all points from that boxcast.
-            RaycastHit[] areaInputs = Physics.BoxCastAll(new Vector3(waypoint.transform.position.x, waypoint.transform.position.y, waypoint.transform.position.z), 
-            new Vector3(boxCastLimits, boxCastLimits, boxCastLimits), -transform.up, Quaternion.identity);
+            WaypointDetection waypointDetection = waypoint.GetComponent<WaypointDetection>();
+            if (zPosWaypoint != null)
+                waypointDetection.nearbyWaypoints.Add(zPosWaypoint);
+            if (zNegWaypoint != null)
+                waypointDetection.nearbyWaypoints.Add(zNegWaypoint);
+            if (xPosWaypoint != null)
+                waypointDetection.nearbyWaypoints.Add(xPosWaypoint);
+            if (xNegWaypoint != null)
+                waypointDetection.nearbyWaypoints.Add(xNegWaypoint);
             
-            //Gets all of the waypoints by filtering through tag.
-            List<RaycastHit> waypointsFound = areaInputs.ToList().FindAll(valid => valid.transform.tag == "Waypoint");
+            xNegWaypoint = null;
+            xPosWaypoint = null;
+            zNegWaypoint = null;
+            zPosWaypoint = null;
 
-            //Way to confirm that the waypoints have indeed been found.
-            Color RandColour = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-
-            //Iterates through all of the raycasthits in the boxcastall.
-            foreach (RaycastHit foundPoint in waypointsFound)
-            {
-                // Changes the colour of hit points in order to verify they've been received.
-                foundPoint.transform.GetComponent<Renderer>().material.color = RandColour;       
-
-                RaycastHit waypointHit;
-                //Draws a line so long as there isn't a wall blocking the raycast.
-                Vector3 dir = (waypoint.transform.position - foundPoint.transform.position).normalized;
-                if (!Physics.Raycast(waypoint.transform.position, dir, out waypointHit, Mathf.Infinity, wallMask))
-                {
-                    if (waypointHit) continue;
-                    Debug.DrawLine(waypoint.transform.position, foundPoint.transform.position, color, 1000f);
-                }
-
-            }*/
-            
-            /*
-            Debug.Log(areaInputs.Length);
-            foreach (RaycastHit hit in areaInputs)
-            {
-                if (hit.transform.tag != "Waypoint") continue;
-                
-
-                if (Physics.Raycast(waypoint.transform.position, hit.transform.position, out RaycastHit waypointHit))
-                {
-                    Debug.Log("aaa");
-                    Debug.DrawLine(waypoint.transform.position, waypointHit.transform.position, color, 1000f);
-
-                    if (waypointHit.transform.tag != "Waypoint") continue;
-                    
-                    //Debug.DrawLine(waypoint.transform.position, waypointHit.transform.position, color, 20f);
-                    
-                }
-                
-            }*/
         }
     }
 
